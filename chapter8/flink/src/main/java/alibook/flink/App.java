dc5d717156d2e9edb2a10a69d7a42c7a2129cc5e
alibook <aliyunbook@gmail.com>
2016-11-21 23:45:53 +0800

    add socket text stream example

---
+++ chapter8/flink/src/main/java/alibook/flink/App.java
@@ -0,0 +1,13 @@
+package alibook.flink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
